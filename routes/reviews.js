9bb4b1f18e81b77670be092d85f1fe37118a4795
BankTNBD <bankthanabadee2007@icloud.com>
2026-04-23 10:08:34 +0700

    feat: implement API to delete a specific rating/comment

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const { createReview, getReviews } = require("../controllers/reviews");
+const { createReview, getReviews, deleteReview } = require("../controllers/reviews");
 
 const router = express.Router({ mergeParams: true });
 const { protect } = require("../middleware/auth");
 
 router.route("/").get(getReviews).post(protect, createReview);
+router.route("/:id").delete(protect, deleteReview);
 
 module.exports = router;
